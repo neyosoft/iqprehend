06dcc37482fdcea787e49239633c5b58f168046c
Obagunwa Emmanuel <access2emma@gmail.com>
2021-08-09 11:17:06 +0100

    added config

--- config.sample.js
+++
@@ -1,9 +0,0 @@
-export default {
-    appName: "iQprehend",
-    appVersion: "0.1.0",
-    environment: "development",
-    DEV_SERVER_URL: "http://192.168.10.103:50987/api",
-    PROD_SERVER_URL: "https://prod.example.com:8443",
-    DEV_PAYSTACK_KEY: "pk_test_a3ed9e085e83b82c53180a8b2578aa624a59ec0a",
-    PROD_PAYSTACK_KEY: "pk_live_a3",
-};
