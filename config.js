0b4d3af93c4a7dab8847946e79aad0f160da40ff
Obagunwa Emmanuel <access2emma@gmail.com>
2021-08-30 16:10:50 +0100

    bump version

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export default {
     appName: "iQprehend",
-    appVersion: "0.1.2",
+    appVersion: "0.1.3",
     environment: "development",
-    DEV_SERVER_URL: "http://192.168.10.106:50987/api",
+    DEV_SERVER_URL: "https://server.iqprehend.herlabytes.com/api",
     PROD_SERVER_URL: "https://server.iqprehend.herlabytes.com/api",
     DEV_PAYSTACK_KEY: "pk_test_a3ed9e085e83b82c53180a8b2578aa624a59ec0a",
     PROD_PAYSTACK_KEY: "pk_live_2db148adfd2d047f38cd0edf131fe657c9203aa3",
