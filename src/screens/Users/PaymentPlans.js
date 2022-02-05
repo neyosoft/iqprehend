08ce0eaf744376e306d3e4a0304c4e5c0d6540f5
Obagunwa Emmanuel <access2emma@gmail.com>
2022-02-06 00:15:43 +0100

    WIP - payment design

--- src/screens/Users/PaymentPlans.js
+++ src/screens/Users/PaymentPlans.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
+import Carousel from "react-native-snap-carousel";
 import { useToast } from "react-native-fast-toast";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RFPercentage } from "react-native-responsive-fontsize";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { View, StyleSheet, TouchableOpacity, StatusBar, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { View, StyleSheet, TouchableOpacity, StatusBar, TouchableWithoutFeedback, Dimensions } from "react-native";
 
 import theme from "../../theme";
 import { useAuth } from "../../context";
-import { moneyFormat } from "../../utils/money.utils";
-import { AppText, Button, PageLoading } from "../../components";
 import { PaymentConfirmationModal } from "../../modals/PaymentConfirmationModal";
+import { AppBoldText, AppMediumText, AppText, Button, PageLoading } from "../../components";
+
+const { width } = Dimensions.get("window");
 
 export const PaymentPlans = ({ navigation }) => {
     const toast = useToast();
@@ -37,6 +39,24 @@ export const PaymentPlans = ({ navigation }) => {
         setShowConfirmModal(true);
     };
 
+    const _renderItem = ({ item }) => {
+        return (
+            <View style={styles.slide}>
+                <AppBoldText style={styles.slideTitle}>{item.name}</AppBoldText>
+                <AppText style={styles.slideItemLabel}>1 Article and video per month</AppText>
+                <AppText style={styles.slideItemLabel}>1 Summary per month</AppText>
+                <AppText style={styles.slideItemLabel}>Win up to N10,000 month</AppText>
+
+                <AppMediumText style={styles.slideItemPriceLabel}>NGN{item.price}</AppMediumText>
+
+                <Button
+                    style={{ paddingHorizontal: 0, marginTop: RFPercentage(4), borderRadius: 6 }}
+                    label="Subscribe Now"
+                />
+            </View>
+        );
+    };
+
     const renderContent = () => {
         if (planResponse.isLoading) {
             return <PageLoading />;
@@ -57,54 +77,32 @@ export const PaymentPlans = ({ navigation }) => {
 
         const plans = planResponse.data;
 
+        const ENTRIES = [
+            {
+                name: "Standard",
+                price: `${plans.STANDARD.price}/Year`,
+            },
+            {
+                name: "Expert",
+                price: `${plans.EXPERT.price}/Month`,
+            },
+            {
+                name: "Premium",
+                price: `${plans.PREMIUM.price}/Year`,
+            },
+        ];
+
         return (
-            <>
-                <ScrollView style={styles.scrollview} contentContainerStyle={styles.contentContainerStyle}>
-                    <TouchableWithoutFeedback onPress={() => setPlan("STANDARD")}>
-                        <View style={[styles.card, plan === "STANDARD" ? styles.activeCardContent : undefined]}>
-                            <View style={styles.cardHeader}>
-                                <AppText style={styles.cardHeaderText}>Standard</AppText>
-                            </View>
-                            <View style={styles.cardContent}>
-                                <AppText style={styles.planPrice}>{moneyFormat(plans.STANDARD.price)}/year</AppText>
-                            </View>
-                        </View>
-                    </TouchableWithoutFeedback>
+            <View style={styles.contentContainer}>
+                <AppText style={styles.introText}>
+                    Thank you for reading. Subcribe to any of our product catergories to submit a summary
+                </AppText>
 
-                    <TouchableWithoutFeedback onPress={() => setPlan("EXPERT")}>
-                        <View
-                            style={[
-                                styles.card,
-                                { marginTop: RFPercentage(4) },
-                                plan === "EXPERT" ? styles.activeCardContent : undefined,
-                            ]}>
-                            <View style={styles.cardHeader}>
-                                <AppText style={styles.cardHeaderText}>Expert</AppText>
-                            </View>
-                            <View style={styles.cardContent}>
-                                <AppText style={styles.planPrice}>{moneyFormat(plans.EXPERT.price)}/month</AppText>
-                            </View>
-                        </View>
-                    </TouchableWithoutFeedback>
+                <View style={styles.sliderWrapper}>
+                    <Carousel data={ENTRIES} renderItem={_renderItem} sliderWidth={width} itemWidth={250} />
+                </View>
 
-                    <TouchableWithoutFeedback onPress={() => setPlan("PREMIUM")}>
-                        <View
-                            style={[
-                                styles.card,
-                                { marginTop: RFPercentage(4) },
-                                plan === "PREMIUM" ? styles.activeCardContent : undefined,
-                            ]}>
-                            <View style={styles.cardHeader}>
-                                <AppText style={styles.cardHeaderText}>Premium</AppText>
-                            </View>
-                            <View style={styles.cardContent}>
-                                <AppText style={styles.planPrice}>{moneyFormat(plans.PREMIUM.price)}/year</AppText>
-                            </View>
-                        </View>
-                    </TouchableWithoutFeedback>
-
-                    <Button label="Proceed" style={styles.proceedBtn} onPress={handlePlanselection} />
-                </ScrollView>
+                {/* <Button label="Proceed" style={styles.proceedBtn} onPress={handlePlanselection} /> */}
 
                 <PaymentConfirmationModal
                     show={showConfirmModal}
@@ -116,13 +114,14 @@ export const PaymentPlans = ({ navigation }) => {
                         navigation.navigate("MakePayment", { plan: planResponse.data[plan] });
                     }}
                 />
-            </>
+            </View>
         );
     };
 
     return (
         <SafeAreaView edges={["top"]} style={{ flex: 1, backgroundColor: theme.colors.primary }}>
             <StatusBar barStyle="light-content" backgroundColor={theme.colors.primary} />
+
             <View style={styles.container}>
                 <View style={styles.header}>
                     <TouchableOpacity onPress={navigation.goBack}>
@@ -141,7 +140,38 @@ export const PaymentPlans = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#C7C7C7",
+        backgroundColor: "#fff",
+    },
+    contentContainer: {
+        padding: RFPercentage(3),
+    },
+    introText: {
+        textAlign: "center",
+        color: theme.colors.primary,
+    },
+    sliderWrapper: {
+        marginTop: RFPercentage(3),
+    },
+    slide: {
+        elevation: 3,
+        borderRadius: 6,
+        backgroundColor: "#fff",
+        padding: RFPercentage(4),
+    },
+    slideTitle: {
+        color: theme.colors.primary,
+        fontSize: RFPercentage(3),
+    },
+    slideItemLabel: {
+        color: theme.colors.primary,
+        marginTop: RFPercentage(2),
+        fontSize: RFPercentage(1.8),
+    },
+    slideItemPriceLabel: {
+        textAlign: "center",
+        fontSize: RFPercentage(2.5),
+        color: theme.colors.primary,
+        marginTop: RFPercentage(3),
     },
     header: {
         flexDirection: "row",
@@ -154,42 +184,6 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontSize: RFPercentage(2.4),
         marginHorizontal: RFPercentage(2),
-    },
-    scrollview: {
-        marginTop: RFPercentage(2),
-    },
-    contentContainerStyle: {
-        padding: RFPercentage(3),
-    },
-    card: {
-        borderRadius: 5,
-        overflow: "hidden",
-        backgroundColor: "#fff",
-        paddingBottom: RFPercentage(3),
-    },
-    cardHeader: {
-        alignItems: "center",
-        paddingVertical: RFPercentage(2),
-        backgroundColor: theme.colors.primary,
-    },
-    cardHeaderText: {
-        color: "#fff",
-    },
-    cardContent: {
-        width: "70%",
-        marginTop: 10,
-        alignSelf: "center",
-    },
-    activeCardContent: {
-        backgroundColor: "#999",
-    },
-    checkbox: {
-        paddingVertical: 4,
-    },
-    planPrice: {
-        fontSize: RFPercentage(4),
-        textAlign: "center",
-        marginVertical: 10,
     },
     proceedBtn: {
         marginTop: RFPercentage(5),
